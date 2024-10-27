44659dded0a86285978fab1a3b0c376475daf4f5
Vadim Omeltchenko <vadimo@amazon.com>
2024-10-27 17:56:00 -0400

    troubleshooting

--- manage-workshop-stack.sh
+++ manage-workshop-stack.sh
@@ -5,7 +5,7 @@ pip install -r requirements.txt
 STACK_OPERATION=$1
 echo "STACK_OPERATION: $STACK_OPERATION"
 
-if [[ "$STACK_OPERATION" == "create" || "$STACK_OPERATION" == "update" ]]; then
+if [[ "$STACK_OPERATION" == "Create" || "$STACK_OPERATION" == "Update" ]]; then
     # deploy / update workshop resources
     python3 create_kb.py
 elif [ "$STACK_OPERATION" == "delete" ]; then
